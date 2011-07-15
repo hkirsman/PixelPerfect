0bf1a4f5ba212a2cb055d5c50c8029aabbe71987
Chris Miller <cmiller@odium.(none)>
2011-07-15 13:57:17 -0700

    intial port to drupal 7

--- pixelperfect.js
+++ pixelperfect.js
@@ -1,116 +1,149 @@
-if (Drupal.jsEnabled) {
-  $(document).ready(function () {
-    strs = Drupal.settings.ppStrings;
+(function($) {
+  $(document).ready(function() {
+    if (Drupal.settings.ppReady) {
+      strs = Drupal.settings.ppStrings;
 
-    var img = new Image();
-    $(img).load();
-    img.src = strs.pp_mockup;
+      var img = new Image();
+      $(img).load().attr('src', strs.pp_mockup);
 
-    $('<div id="pixelperfect-overlay"></div>')
-      .appendTo('body')
-      .css({
-        'background-image': 'url('+ strs.pp_mockup +')', 
-        'top' : strs.pp_top, 
-        'left' : strs.pp_left, 
-        'width' : img.width, 
-        'height' : img.height
-      })
-      .mouseup(function () {
-        $('#pp_top').val($('#pixelperfect-overlay').css('top'));
-        $('#pp_left').val($('#pixelperfect-overlay').css('left'));
-      })
-      .draggable({
-        stop: function(event, ui) {
-          jQuery.post(Drupal.settings.basePath + 'pixelperfect/update-position', {
-            top: $('#pixelperfect-overlay').css('top'),
-            left: $('#pixelperfect-overlay').css('left'),
-            id: strs.pp_mockup_id 
-          });
-        }
+      $(window).load(function() {
+        $('<div id="pixelperfect-overlay-wrapper"><div id="pixelperfect-overlay"></div></div>').appendTo('body');
+
+        $('#pixelperfect-overlay')
+          .draggable({ opacity: 0.5 })
+          .css({
+            'background-image': 'url('+ encodeURI(strs.pp_mockup) +')', 
+            'top' : strs.pp_top, 
+            'left' : strs.pp_left, 
+            'width' : img.width, 
+            'height' : img.height
+          })
+          .mouseup(function () {
+            $('#pp_top').val($('#pixelperfect-overlay').css('top'));
+            $('#pp_left').val($('#pixelperfect-overlay').css('left'));
+
+            jQuery.post(Drupal.settings.basePath + 'pixelperfect/update-position', {
+              top: $('#pixelperfect-overlay').css('top'),
+              left: $('#pixelperfect-overlay').css('left'),
+              id: $('#pp-options').val() 
+            });
+          })
+          .fadeTo(0, 0.5);
       });
 
-    x = 1;
-    d = {};
+      x = 1; 
+      d = {};
 
-    function newv(v,a,b) {
-        $('#pp_top').val($('#pixelperfect-overlay').css('top'));
-        $('#pp_left').val($('#pixelperfect-overlay').css('left'));
+      function newv(v,a,b) {
+          $('#pp_top').val($('#pixelperfect-overlay').css('top'));
+          $('#pp_left').val($('#pixelperfect-overlay').css('left'));
 
-        var n = parseInt(v, 10) - (d[a] ? x : 0) + (d[b] ? x : 0);
-        return n;
-    }
-    
-    var oEnabled = 0;
-    var oEnable = function() {
-      oEnabled = 1 - oEnabled;
-      if(oEnabled) {
-        $(window).keydown(function(e) { 
-          d[e.which] = true; 
-          if(oEnabled) {
-            e.preventDefault();
-          }
-        });
-        $(window).keyup(function(e) { 
-          if(oEnabled) {
-            d[e.which] = false; 
-            if(e.which >= 37 && e.which <= 40) {
-              jQuery.post(Drupal.settings.basePath + 'pixelperfect/update-position', {
-                top: $('#pixelperfect-overlay').css('top'),
-                left: $('#pixelperfect-overlay').css('left'),
-                id: strs.pp_mockup_id 
-              });
-              console.log('f');
+          var n = parseInt(v, 10) - (d[a] ? x : 0) + (d[b] ? x : 0);
+          return n;
+      }
+      
+      var oEnabled = 0;
+      var oEnable = function() {
+        oEnabled = 1 - oEnabled;
+        if(oEnabled) {
+          $(window).keydown(function(e) { 
+            if(oEnabled && e.which >= 37 && e.which <= 40) {
+              d[e.which] = true; 
               e.preventDefault();
             }
-          } else {
-            //console.log('t');
-            //return true;
-            //$(window).keydown(function(e) { d[e.which] = false; return true; });
-            //$(window).keyup(function(e) { return true; }); 
-          }
-        });
-      }
-    }
-
-    setInterval(function() {
-        $('#pixelperfect-overlay').css({
-            left: function(i,v) { 
-              return newv($(this).css('left'), 37, 39); 
-            },
-            top: function(i,v) { 
-              return newv($(this).css('top'), 38, 40); 
+          });
+          $(window).keyup(function(e) { 
+            if(oEnabled) {
+              d[e.which] = false; 
+              if(e.which >= 37 && e.which <= 40) {
+                jQuery.post(Drupal.settings.basePath + 'pixelperfect/update-position', {
+                  top: $('#pixelperfect-overlay').css('top'),
+                  left: $('#pixelperfect-overlay').css('left'),
+                  id: $('#pp-options').val()
+                });
+                e.preventDefault();
+              }
             }
+          });
+        }
+      }
+
+      setInterval(function() {
+          $('#pixelperfect-overlay').css({
+              left: function(i,v) { 
+                return newv($(this).css('left'), 37, 39); 
+              },
+              top: function(i,v) { 
+                return newv($(this).css('top'), 38, 40); 
+              }
+          });
+      }, 100);
+      $('<div id="pixelperfect-toggle"><input id="pp_top" type="hidden" value="'+ strs.pp_top +'"><input id="pp_left" type="hidden" value="'+ strs.pp_left +'" /></div>')
+        .appendTo($('body'));
+      $('<select id="pp-options">' + strs.pp_options + '</select>')  
+        .appendTo($('#pixelperfect-toggle'))
+        .change(function() {
+          jQuery.post(Drupal.settings.basePath + 'pixelperfect/load-mockup', {
+            id: $('#pp-options').val()
+          },
+          function (d,t,x) {
+            response = jQuery.parseJSON(d);
+            strs.pp_mockup = response.mockup;
+            strs.pp_mockup_invert = response.mockup_invert;
+            strs.pp_top = response.mockup_top;
+            strs.pp_left = response.mockup_left;
+            img.src = response.mockup;
+
+            $(img).load(function() {
+              $('#pixelperfect-overlay')
+                .css({
+                  'background-image': 'url('+ encodeURI(strs.pp_mockup) +')',
+                  'top' : strs.pp_top,
+                  'left' : strs.pp_left,
+                  'width' : img.width,
+                  'height' : img.height
+                });
+              });
+          });
         });
-    }, 100);
-    $('<div id="pixelperfect-toggle"><input id="pp_top" type="hidden" value="'+ strs.pp_top +'"><input id="pp_left" type="hidden" value="'+ strs.pp_left +'" /></div>')
-      .appendTo($('body'));
-    $('<label for="pp_toggle">'+ strs.pp_toggle +'</label><input id="pp_toggle" type="checkbox" />')
-      .appendTo($('#pixelperfect-toggle'))
-      .click(function() {
-        ppMockup(); 
-        oEnable();
-      });
-    $('<label for="pp_invert">'+ strs.pp_invert +'</label><input id="pp_invert" type="checkbox" />')
-      .appendTo($('#pixelperfect-toggle'))
-      .click(function() {
-        ppInvert(); 
-        oEnable();
-      });
+      $('<label for="pp_toggle">'+ strs.pp_toggle +'</label><input id="pp_toggle" type="checkbox" />')
+        .appendTo($('#pixelperfect-toggle'))
+        .click(function() {
+          ppMockup(); 
+          oEnable();
+        });
+      $('<label for="pp_invert">'+ strs.pp_invert +'</label><input id="pp_invert" type="checkbox" />')
+        .appendTo($('#pixelperfect-toggle'))
+        .click(function() {
+          ppInvert(); 
+          oEnable();
+        });
+    }
   });
 
   var ppMockupEnabled = 0;
+  var ppInvertEnabled = 0;
+
   var ppMockup = function() {
     ppMockupEnabled = 1 - ppMockupEnabled;
     $('#pp_toggle').attr('checked', ppMockupEnabled ? 'checked' : '');
+    ppInvertEnabled = 0;
+    $('#pp_invert').attr('checked','');
     $('#pixelperfect-overlay')
-      .css({'background-image': 'url('+ strs.pp_mockup+')', 'display' : ppMockupEnabled ? 'block' : 'none'});
+      .css({'background-image': 'url('+ encodeURI(strs.pp_mockup) +')'});
+    $('#pixelperfect-overlay-wrapper')
+      .css({'display' : ppMockupEnabled ? 'block' : 'none'});
   };
 
-  var ppInvertEnabled = 0;
   var ppInvert = function() {
     ppInvertEnabled = 1 - ppInvertEnabled;
     $('#pp_invert').attr('checked', ppInvertEnabled ? 'checked' : '');
+    ppMockupEnabled = 0;
+    $('#pp_toggle').attr('checked','');
     $('#pixelperfect-overlay')
-      .css({'background-image': 'url('+ strs.pp_mockup_invert +')', 'display' : ppInvertEnabled ? 'block' : 'none'});
+      .css({'background-image': 'url('+ encodeURI(strs.pp_mockup_invert) +')'});
+    $('#pixelperfect-overlay-wrapper')
+      .css({'display' : ppInvertEnabled ? 'block' : 'none'});
   };
-}
+
+})(jQuery);
